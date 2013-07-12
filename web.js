3dc6344be9abeda495c99e201c7456441700136c
tonisubi <toni.subirana@gmail.com>
2013-07-12 12:03:43 +0000

    buffer

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indx = fs.readFileSync('index.html');
+  
+  response.send(indx.toString());
 });
 
 var port = process.env.PORT || 5000;
